6ddd3f692706eecddf70aacd0ed375b277c508bd
krekoster <72513456+krekoster@users.noreply.github.com>
2023-07-31 12:13:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function toggleLinksDisplay () {
-  
     let menu = document.getElementById("myLinks");
     let background = document.getElementById("content");
     // background.style.backgroundColor = "red";
     if (menu.style.display === "block") {
         menu.style.display = "none"; 
-       
         }
     else {
         menu.style.display = "block";
         //background.style.backgroundColor = "red";
-       
-
     }
 }
 
