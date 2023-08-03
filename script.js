165102082f869380004b616f08ac8ee612ea1aec
krekoster <72513456+krekoster@users.noreply.github.com>
2023-08-03 16:36:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function toggleLinksDisplay () {
+function toggleLinksDisplay() {
     let menu = document.getElementById("myLinks");
     let background = document.getElementById("content");
     // background.style.backgroundColor = "red";
     if (menu.style.display === "block") {
-        menu.style.display = "none"; 
-        }
+        menu.style.display = "none";
+    }
     else {
         menu.style.display = "block";
         //background.style.backgroundColor = "red";
@@ -12,4 +12,18 @@ function toggleLinksDisplay () {
 }
 
 
+
+$("img.preview_slikoprice").click(function(){
+    // let izlaz = $(this).css("width");
+    // console.log(izlaz);
+
+    if ($(this).css("width") === "60px") {
+        $(this).css("width","99vw");
+        $(".naziv_slikoprice").hide();
+    }
+    else {
+        $(this).css("width","60px");
+        $(".naziv_slikoprice").show();
+    }
+})  ;  
 
