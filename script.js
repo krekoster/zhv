dfca659400c310d67192a5053f9c7cba3e1e89ec
krekoster <72513456+krekoster@users.noreply.github.com>
2023-08-03 20:00:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,21 +13,23 @@ function toggleLinksDisplay() {
 
 
 
-$("img.preview_slikoprice").click(function(){
-    if ($(this).css("width") === "60px") {
-        if ($(window).width() < 800) {
-            $(this).css({"width": "95vw"});
-            $(".naziv_slikoprice").hide();
+ 
+
+$(document).ready(function () {
+    $("img.preview_slikoprice").click(function(){
+        if ($(this).css("width") === "60px") {
+            if ($(window).width() < 800) {
+                $(this).css({"width": "95vw"});
+                $(".naziv_slikoprice").hide();
+            }
+            else {
+                $(this).css({"width": "50vw"});
+                $(".naziv_slikoprice").hide();
+            } 
         }
         else {
-            $(this).css({"width": "50vw"});
-            $(".naziv_slikoprice").hide();
+            $(this).css("width","60px");
+            $(".naziv_slikoprice").show();
         }
-        
-    }
-    else {
-        $(this).css("width","60px");
-        $(".naziv_slikoprice").show();
-    }
-})  ;  
-
+    })  ; 
+  });
